bee19556b7f769110d098feded407620fe280214
Vuk Isic <visicev@gmail.com>
2020-09-18 16:54:23 +0200

    example of server-side app finished

---
+++ Blazor_ServerSideApp/Blazor_ServerSideApp/Data/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_ServerSideApp.Data
+{
+    public class TaskModel
+    {
+        public string Task { get; set; }
+        public bool Complete { get; set; }
+    }
+}
